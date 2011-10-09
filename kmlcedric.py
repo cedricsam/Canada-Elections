f642a9caeab89b49fc6d3bf8953e57364e7e8fa7
Cedric Sam <cedricsam@gmail.com>
2011-10-09 19:17:03 +0800

    putting actual files

--- kmlcedric.py
+++ kmlcedric.py
@@ -1,1 +1,146 @@
-/home/csam/fed2011/kmlcedric.py
+#-*- coding: utf-8
+import kmlbase
+import kmldom
+import kmlengine
+
+"""
+@factory - libkml factory
+@id - id of style to use
+"""
+def genBasicStyle(id):
+    factory = kmldom.KmlFactory_GetFactory()
+    style = factory.CreateStyle()
+    style.set_id(id)
+    # Create style objects
+    iconStyle = factory.CreateIconStyle()
+    labelStyle = factory.CreateLabelStyle()
+    lineStyle = factory.CreateLineStyle()
+    polyStyle = factory.CreatePolyStyle()
+    balloonStyle = factory.CreateBalloonStyle()
+    listStyle = factory.CreateListStyle()
+    # Set styles
+    style.set_iconstyle(iconStyle)
+    style.set_labelstyle(labelStyle)
+    style.set_linestyle(lineStyle)
+    style.set_polystyle(polyStyle)
+    style.set_balloonstyle(balloonStyle)
+    style.set_liststyle(listStyle)
+    return style
+
+def customPolyStyle(polyStyle, color):
+    polyStyle.set_color(kmlbase.Color32(color))
+    polyStyle.set_colormode(kmldom.COLORMODE_NORMAL)
+    return polyStyle
+
+def standardBalloonStyle(balloonStyle):
+    balloonStyleText = '<h1>$[name]</h1><h2>$[Snippet]</h2>$[description]'
+    balloonStyle.set_text(balloonStyleText)
+    return balloonStyle
+
+def noTitleBalloonStyle(balloonStyle):
+    balloonStyleText = '$[description]'
+    balloonStyle.set_text(balloonStyleText)
+    return balloonStyle
+
+def get_gradient_color(colorA, colorB, val):
+    # 'val' must be between 0 and 1
+    color = [0, 0, 0]
+    for i in range(len(color)):
+	color[i] = colorA[i] + val * (colorB[i] - colorA[i])
+    return color
+
+def partyColor(partyName):
+    if partyName == 'Liberal':
+	color = 0x0000ff
+    elif partyName == 'Conservative':
+	color = 0xff0000
+    elif partyName == 'NDP-New Democratic Party' or partyName == 'N.D.P.':
+	color = 0x008cff
+    elif partyName == 'Bloc Québécois':
+	color = 0x808000
+    elif partyName == 'Green Party':
+	color = 0x006400
+    else:
+	color = 0xffffff
+    return color
+
+def getPartyIdByName(partyName):
+    if partyName == 'Liberal':
+	partyId = 2
+    elif partyName == 'Conservative':
+	partyId = 3
+    elif partyName == 'NDP-New Democratic Party' or partyName == 'N.D.P.':
+	partyId = 4
+    elif partyName == 'Bloc Québécois':
+	partyId = 5
+    elif partyName == 'Green Party':
+	partyId = 6
+    else:
+	partyId = 0
+    return partyId
+
+def getPartyNameById(partyId):
+    if partyId == 2:
+	partyName = 'Liberal'
+    elif partyId == 3:
+       	partyName = 'Conservative'
+    elif partyId == 4:
+   	partyName = 'NDP-New Democratic Party'
+    elif partyId == 5:
+  	partyName = 'Bloc Québécois'
+    elif partyId == 6:
+ 	partyName = 'Green Party'
+    else:
+	partyName = None
+    return partyName
+
+def getHtmlHexFromGEHex(GEHex, hashmark=True):
+    if GEHex > 0x1000000:
+	i = 2
+    else:
+	i = 0
+    B = hex(GEHex)[2+i:4+i]
+    G = hex(GEHex)[4+i:6+i]
+    R = hex(GEHex)[6+i:8+i]
+    if not len(B):
+	R = "00"
+	G = "00"
+	B = "00"
+    elif not len(G):
+	R = B
+	G = "00"
+	B = "00"
+    elif not len(R):
+	R = G
+	G = B
+	B = "00"
+    if hashmark:
+	return "#" + R + G + B
+    else:
+	return R + G + B
+
+def getHtmlHexFromGEHex(GEHex, hashmark=True):
+    if GEHex > 0x1000000:
+	i = 2
+    else:
+	i = 0
+    B = hex(GEHex)[2+i:4+i]
+    G = hex(GEHex)[4+i:6+i]
+    R = hex(GEHex)[6+i:8+i]
+    if not len(B):
+	R = "00"
+	G = "00"
+	B = "00"
+    elif not len(G):
+	R = B
+	G = "00"
+	B = "00"
+    elif not len(R):
+	R = G
+	G = B
+	B = "00"
+    if hashmark:
+	return "#" + R + G + B
+    else:
+	return R + G + B
+
--- updatewater.sh
+++ updatewater.sh
@@ -1,1 +1,6 @@
-/home/csam/fed2011/updatewater.sh
+#!/bin/bash
+
+while read i
+do echo $i
+    psql -U canada -h 127.0.0.1 -c "update pd308_a_2011 p set the_geom_nowater = foo.nowater from (select p.pd_num, p.emrp_name, ST_Intersection(ST_Transform(p.the_geom, 4326), f.the_geom) nowater from pd308_a_2011 p left join fed308_a_nowater f on p.fed_num = cast(f.feduid as integer) where fed_num = $i) foo WHERE p.fed_num = '$i' AND p.pd_num = foo.pd_num AND p.emrp_name = foo.emrp_name "
+done < kmlall.txt
